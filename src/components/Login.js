69664dde3e9dae9b504591af27db07fc234f4a99
MariaAddict <mashakan27@gmail.com>
2020-12-10 20:29:03 +0700

    fix: исправить инпуты в наборе пароля

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ function Login(props) {
             <form action="#" name="login-form" className= "login__form" onSubmit={props.onSubmit} >
                 <input type="email" name="email" className="login__input login__input_type_email"
                     placeholder="Email" /*onChange={handleChangeEmail}*/  />
-                <input type="text" name="password" className="login__input login__input_type_password"
+                <input type="password" name="password" className="login__input login__input_type_password"
                     placeholder="Пароль" /*onChange={handleChangePassword} */  />
                 <button type="submit" className="login__submit-button">Войти</button>
             </form>
